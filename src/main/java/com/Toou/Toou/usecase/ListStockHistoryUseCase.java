eded37cbaab2fcf1b0cbdb9d8a827df834d6c7ef
HyunSangHan <gustkd3@gmail.com>
2024-08-04 00:08:49 +0900

    feat: 주식 히스토리 목록 조회 유스케이스 인터페이스 정의

---
+++ src/main/java/com/Toou/Toou/usecase/ListStockHistoryUseCase.java
@@ -0,0 +1,27 @@
+package com.Toou.Toou.usecase;
+
+import com.Toou.Toou.domain.model.StockDailyHistory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ListStockHistoryUseCase {
+
+    Output execute(Input input);
+
+    @AllArgsConstructor
+    @Data
+    class Input {
+        String companyCode;
+        LocalDate dateFrom;
+        LocalDate dateTo;
+    }
+
+    @AllArgsConstructor
+    @Data
+    class Output {
+        List<StockDailyHistory> dailyHistories;
+    }
+}
