d8f70e217570ef97333627f901c0bb0040ff73a9
hunnypooh <jiwon2461999@gmail.com>
2024-08-08 01:56:55 +0900

    feat: stock history dto 변경 [#9]

--- src/main/java/com/Toou/Toou/port/in/dto/StockDailyHistoryDto.java
+++ src/main/java/com/Toou/Toou/port/in/dto/StockDailyHistoryDto.java
@@ -1,19 +1,22 @@
 package com.Toou.Toou.port.in.dto;
 
 import com.Toou.Toou.domain.model.StockDailyHistory;
-import lombok.AllArgsConstructor;
-
 import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class StockDailyHistoryDto {
-    private LocalDate date; // 해당 날짜
-    private Long price; // 해당 날짜의 종가(KRW)
 
-    public static StockDailyHistoryDto fromDomainModel(StockDailyHistory domainModel) {
-        return new StockDailyHistoryDto(
-            domainModel.getDate(),
-            domainModel.getPrice()
-        );
-    }
+	private LocalDate date; // 해당 날짜
+	private List<Long> prices; // 해당 날짜의 종가(KRW)
+
+	public static StockDailyHistoryDto fromDomainModel(StockDailyHistory domainModel) {
+		return new StockDailyHistoryDto(
+				domainModel.getDate(),
+				domainModel.getPrices()
+		);
+	}
 }
--- src/main/java/com/Toou/Toou/port/in/dto/StockHistoryListResponse.java
+++ src/main/java/com/Toou/Toou/port/in/dto/StockHistoryListResponse.java
@@ -1,5 +1,6 @@
 package com.Toou.Toou.port.in.dto;
 
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +9,12 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 public class StockHistoryListResponse {
-    private Long id;
-    private String companyCode;
-    private List<StockDailyHistoryDto> dailyHistories;
+
+	private boolean ok;
+	private Long id;
+	private String stockCode;
+	private String stockName;
+	private Long stockNewestPrice;
+	private LocalDate newestDate;
+	private List<StockDailyHistoryDto> dailyHistories;
 }
