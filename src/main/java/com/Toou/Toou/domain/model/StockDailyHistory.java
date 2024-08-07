9caddee4572595f323b24891cbb76e65d4197f06
hunnypooh <jiwon2461999@gmail.com>
2024-08-08 01:53:50 +0900

    feat: stock history 도메인 모델 변경 [#9]

--- src/main/java/com/Toou/Toou/domain/model/StockDailyHistory.java
+++ src/main/java/com/Toou/Toou/domain/model/StockDailyHistory.java
@@ -1,17 +1,19 @@
 package com.Toou.Toou.domain.model;
 
+import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class StockDailyHistory {
-    private Long id;
-    private String companyCode;
-    private Long price; // 해당 날짜의 종가(KRW)
-    private LocalDate date; // 해당 날짜
+
+	private Long id;
+	private String stockCode;
+	private String stockName;
+	private List<Long> prices; // 시가, 고가, 저가, 종가
+	private LocalDate date; // 해당 날짜
 }
