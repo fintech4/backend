ac19a60a8f9c9a1d2d4b3c567f28268c7c969181
hunnypooh <jiwon2461999@gmail.com>
2024-08-08 02:10:54 +0900

    feat: stock 검색 도메인 모델 [#9]

---
+++ src/main/java/com/Toou/Toou/domain/model/StockMetadata.java
@@ -0,0 +1,15 @@
+package com.Toou.Toou.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class StockMetadata {
+
+	private Long id;
+	private String stockCode;
+	private String stockName;
+}
