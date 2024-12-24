c3544225ec0ccf40accec912f083ae42065ae89e
hunnypooh <jiwon2461999@gmail.com>
2024-12-25 03:39:43 +0900

    fix: 전량 매도할 때에도 HoldingIndividualStock 리턴 [#49] - stockOrder에서 전량 매도 시 HoldingIndividualStock 를 null로 리턴하는데, 그걸 받아서 delete하고 있는 것을 확인. 매도 시 발생한 오류의 원인으로 추정. - null 대신 0으로 값이 채워진 HoldingIndividualStock 을 리턴하고, 리턴받은 HoldingIndividualStock의 id로 delete 되도록 수정.

--- src/main/java/com/Toou/Toou/domain/model/HoldingIndividualStock.java
+++ src/main/java/com/Toou/Toou/domain/model/HoldingIndividualStock.java
@@ -9,89 +9,105 @@ import lombok.NoArgsConstructor;
 @Getter
 public class HoldingIndividualStock {
 
-	private Long id;
-	private String stockCode;              // 종목 코드
-	private String stockName;              // 종목명
-	private Long averageBuyPrice;     // 평균 매수가
-	private Long currentPrice;             // 현재 가격
-	private Long quantity;                 // 보유 주식수
-	private Long valuation;                // 평가금액
-	private Double yield;
-	private Long accountAssetId;
-	private Long principal;                 //투자 원금
+  private Long id;
+  private String stockCode;              // 종목 코드
+  private String stockName;              // 종목명
+  private Long averageBuyPrice;     // 평균 매수가
+  private Long currentPrice;             // 현재 가격
+  private Long quantity;                 // 보유 주식수
+  private Long valuation;                // 평가금액
+  private Double yield;
+  private Long accountAssetId;
+  private Long principal;                 //투자 원금
 
-	public HoldingIndividualStock(StockOrder stockOrder, long accountAssetId) {
-		this.stockCode = stockOrder.getStockCode();
-		this.stockName = stockOrder.getStockName();
-		this.averageBuyPrice = stockOrder.getStockPrice();
-		this.currentPrice = stockOrder.getStockPrice();
-		this.quantity = stockOrder.getOrderQuantity();
-		this.valuation = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-		this.yield = 0.0; // 초기 수익률은 0으로 설정
-		this.accountAssetId = accountAssetId;
-		this.principal = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-	}
+  public HoldingIndividualStock(StockOrder stockOrder, long accountAssetId) {
+    this.stockCode = stockOrder.getStockCode();
+    this.stockName = stockOrder.getStockName();
+    this.averageBuyPrice = stockOrder.getStockPrice();
+    this.currentPrice = stockOrder.getStockPrice();
+    this.quantity = stockOrder.getOrderQuantity();
+    this.valuation = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+    this.yield = 0.0; // 초기 수익률은 0으로 설정
+    this.accountAssetId = accountAssetId;
+    this.principal = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+  }
 
-	public HoldingIndividualStock updateWhenBuyStock(StockOrder stockOrder) {
-		long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-		Long newQuantity = this.quantity + stockOrder.getOrderQuantity();
-		Long newValuation = this.valuation + totalOrderValue;
-		Long newPrincipal = this.principal + totalOrderValue;
-		Long newAverageBuyPrice = newPrincipal / newQuantity;
-		Double newYield =
-				((double) (this.currentPrice - newAverageBuyPrice) / newAverageBuyPrice) * 100;
-		return new HoldingIndividualStock(
-				this.id,
-				this.stockCode,
-				this.stockName,
-				newAverageBuyPrice,
-				this.currentPrice,
-				newQuantity,
-				newValuation,
-				newYield,
-				this.accountAssetId,
-				newPrincipal
-		);
-	}
+  public HoldingIndividualStock updateWhenBuyStock(StockOrder stockOrder) {
+    long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+    Long newQuantity = this.quantity + stockOrder.getOrderQuantity();
+    Long newValuation = this.valuation + totalOrderValue;
+    Long newPrincipal = this.principal + totalOrderValue;
+    Long newAverageBuyPrice = newPrincipal / newQuantity;
+    Double newYield =
+        ((double) (this.currentPrice - newAverageBuyPrice) / newAverageBuyPrice) * 100;
+    return new HoldingIndividualStock(
+        this.id,
+        this.stockCode,
+        this.stockName,
+        newAverageBuyPrice,
+        this.currentPrice,
+        newQuantity,
+        newValuation,
+        newYield,
+        this.accountAssetId,
+        newPrincipal
+    );
+  }
 
-	public HoldingIndividualStock updateWhenSellStock(StockOrder stockOrder) {
-		long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-		Long newQuantity = this.quantity - stockOrder.getOrderQuantity();
-		Long newValuation = this.valuation - totalOrderValue;
-		Long newPrincipal = this.principal - totalOrderValue;
-		Long newAverageBuyPrice = newPrincipal / newQuantity;
-		Double newYield =
-				((double) (this.currentPrice - newAverageBuyPrice) / newAverageBuyPrice) * 100;
-		return new HoldingIndividualStock(
-				this.id,
-				this.stockCode,
-				this.stockName,
-				newAverageBuyPrice,
-				this.currentPrice,
-				newQuantity,
-				newValuation,
-				newYield,
-				this.accountAssetId,
-				newPrincipal
-		);
-	}
+  public HoldingIndividualStock updateWhenSellStock(StockOrder stockOrder) {
+    long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+    Long newQuantity = this.quantity - stockOrder.getOrderQuantity();
+    if (newQuantity == 0) {
+      // 전량 매도 상태를 나타내는 객체 반환
+      return new HoldingIndividualStock(
+          this.id,
+          this.stockCode,
+          this.stockName,
+          this.averageBuyPrice,
+          this.currentPrice,
+          0L,
+          0L,  // 평가 금액 0
+          0.0,
+          this.accountAssetId,
+          0L
+      );
+    }
 
-	public HoldingIndividualStock updateWithNewHoldingsData(Long newPrice) {
-		Long newValuation = newPrice * this.quantity;
-		Double newYield =
-				((double) (this.currentPrice - this.averageBuyPrice) / this.averageBuyPrice) * 100;
+    Long newValuation = this.valuation - totalOrderValue;
+    Long newPrincipal = this.principal - totalOrderValue;
+    Long newAverageBuyPrice = newPrincipal / newQuantity;
+    Double newYield =
+        ((double) (this.currentPrice - newAverageBuyPrice) / newAverageBuyPrice) * 100;
+    return new HoldingIndividualStock(
+        this.id,
+        this.stockCode,
+        this.stockName,
+        newAverageBuyPrice,
+        this.currentPrice,
+        newQuantity,
+        newValuation,
+        newYield,
+        this.accountAssetId,
+        newPrincipal
+    );
+  }
 
-		return new HoldingIndividualStock(
-				this.id,
-				this.stockCode,
-				this.stockName,
-				this.averageBuyPrice,
-				newPrice,
-				this.quantity,
-				newValuation,
-				newYield,
-				this.accountAssetId,
-				this.principal
-		);
-	}
+  public HoldingIndividualStock updateWithNewHoldingsData(Long newPrice) {
+    Long newValuation = newPrice * this.quantity;
+    Double newYield =
+        ((double) (this.currentPrice - this.averageBuyPrice) / this.averageBuyPrice) * 100;
+
+    return new HoldingIndividualStock(
+        this.id,
+        this.stockCode,
+        this.stockName,
+        this.averageBuyPrice,
+        newPrice,
+        this.quantity,
+        newValuation,
+        newYield,
+        this.accountAssetId,
+        this.principal
+    );
+  }
 }
