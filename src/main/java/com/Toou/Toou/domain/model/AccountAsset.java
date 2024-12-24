6e8ef5bc7e85f6fff8b1875b779e5e731b80b20e
hunnypooh <jiwon2461999@gmail.com>
2024-12-25 03:40:31 +0900

    comment: AccountAsset 필드 설명 주석 추가 [#49]

--- src/main/java/com/Toou/Toou/domain/model/AccountAsset.java
+++ src/main/java/com/Toou/Toou/domain/model/AccountAsset.java
@@ -11,77 +11,77 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AccountAsset {
 
-	private Long id;
-	private OAuthProvider oauthProvider; // OAuth 제공자 타입
-	private String providerId; // OAuth 제공자에서의 유저 ID
-	private Long totalAsset;
-	private Long deposit;
-	private Long totalHoldingsValue;
-	private Long totalHoldingsQuantity;
-	private Double investmentYield;
-	private Long totalPrincipal;
+  private Long id;
+  private OAuthProvider oauthProvider; // OAuth 제공자 타입
+  private String providerId; // OAuth 제공자에서의 유저 ID
+  private Long totalAsset; //총 자산 = 현금 + 주식
+  private Long deposit;
+  private Long totalHoldingsValue;
+  private Long totalHoldingsQuantity;
+  private Double investmentYield;
+  private Long totalPrincipal; //총 종목(주식 종류) 수
 
-	public AccountAsset updateWhenBuyStock(StockOrder stockOrder, boolean isFirstBuy) {
-		Long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-		Long newDeposit = this.deposit - totalOrderValue;
-		Long newTotalHoldingsValue = this.totalHoldingsValue + totalOrderValue;
-		Long newTotalHoldingsQuantity = isFirstBuy
-				? this.totalHoldingsQuantity + 1
-				: this.totalHoldingsQuantity;
-		Long newPrincipal = this.totalPrincipal + newTotalHoldingsValue;
+  public AccountAsset updateWhenBuyStock(StockOrder stockOrder, boolean isFirstBuy) {
+    Long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+    Long newDeposit = this.deposit - totalOrderValue;
+    Long newTotalHoldingsValue = this.totalHoldingsValue + totalOrderValue;
+    Long newTotalHoldingsQuantity = isFirstBuy
+        ? this.totalHoldingsQuantity + 1
+        : this.totalHoldingsQuantity;
+    Long newPrincipal = this.totalPrincipal + newTotalHoldingsValue;
 
-		return new AccountAsset(
-				this.id,
-				this.oauthProvider,
-				this.providerId,
-				this.totalAsset,
-				newDeposit,
-				newTotalHoldingsValue,
-				newTotalHoldingsQuantity,
-				this.investmentYield,
-				newPrincipal
-		);
-	}
+    return new AccountAsset(
+        this.id,
+        this.oauthProvider,
+        this.providerId,
+        this.totalAsset,
+        newDeposit,
+        newTotalHoldingsValue,
+        newTotalHoldingsQuantity,
+        this.investmentYield,
+        newPrincipal
+    );
+  }
 
 
-	public AccountAsset updateWhenSellStock(StockOrder stockOrder, boolean isLastStockSold) {
-		Long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
-		Long newDeposit = this.deposit + totalOrderValue;
-		Long newTotalHoldingsValue = this.totalHoldingsValue - totalOrderValue;
-		Long newTotalHoldingsQuantity = isLastStockSold
-				? this.totalHoldingsQuantity - 1
-				: this.totalHoldingsQuantity;
-		Long newPrincipal = this.totalPrincipal - newTotalHoldingsValue;
+  public AccountAsset updateWhenSellStock(StockOrder stockOrder, boolean isLastStockSold) {
+    Long totalOrderValue = stockOrder.getStockPrice() * stockOrder.getOrderQuantity();
+    Long newDeposit = this.deposit + totalOrderValue;
+    Long newTotalHoldingsValue = this.totalHoldingsValue - totalOrderValue;
+    Long newTotalHoldingsQuantity = isLastStockSold
+        ? this.totalHoldingsQuantity - 1
+        : this.totalHoldingsQuantity;
+    Long newPrincipal = this.totalPrincipal - newTotalHoldingsValue;
 
-		return new AccountAsset(
-				this.id,
-				this.oauthProvider,
-				this.providerId,
-				this.totalAsset,
-				newDeposit,
-				newTotalHoldingsValue,
-				newTotalHoldingsQuantity,
-				this.investmentYield,
-				newPrincipal
-		);
-	}
+    return new AccountAsset(
+        this.id,
+        this.oauthProvider,
+        this.providerId,
+        this.totalAsset,
+        newDeposit,
+        newTotalHoldingsValue,
+        newTotalHoldingsQuantity,
+        this.investmentYield,
+        newPrincipal
+    );
+  }
 
-	public AccountAsset updateWithNewHoldingsData(Long totalHoldingsValue) {
-		long newTotalAsset = this.deposit + totalHoldingsValue;
-		double newInvestmentYield =
-				((double) (newTotalAsset - this.totalPrincipal) / this.totalPrincipal) * 100;
+  public AccountAsset updateWithNewHoldingsData(Long totalHoldingsValue) {
+    long newTotalAsset = this.deposit + totalHoldingsValue;
+    double newInvestmentYield =
+        ((double) (newTotalAsset - this.totalPrincipal) / this.totalPrincipal) * 100;
 
-		return new AccountAsset(
-				this.id,
-				this.oauthProvider,
-				this.providerId,
-				newTotalAsset,
-				this.deposit,
-				totalHoldingsValue,
-				this.totalHoldingsQuantity,
-				newInvestmentYield,
-				this.totalPrincipal
-		);
-	}
+    return new AccountAsset(
+        this.id,
+        this.oauthProvider,
+        this.providerId,
+        newTotalAsset,
+        this.deposit,
+        totalHoldingsValue,
+        this.totalHoldingsQuantity,
+        newInvestmentYield,
+        this.totalPrincipal
+    );
+  }
 
 }
